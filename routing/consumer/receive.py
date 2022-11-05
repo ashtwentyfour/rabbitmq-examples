eca16bff46c7bd712de10ce9f4eade3417c6438b
Ashwin Menon <ashtwentyfour@Ashwins-MacBook-Air.local>
2022-11-05 11:57:12 -0400

    added direct routing example

---
+++ routing/consumer/receive.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+'''Job which consumes RabbitMQ queue messages from a direct exchange'''
+
+import os
+import sys
+import json
+import pika
+
+def callback(ch, method, properties, body):
+    '''listener callback function'''
+    print("consumed message => " + str(body))
+
+# credentials
+rabbit_user = os.getenv("RMQ_USER")
+rabbit_password = os.getenv("RMQ_PASSWORD")
+# direct exchange name
+rabbit_exchange = os.getenv("RMQ_EXCHANGE") or 'directex01'
+
+# connection specs
+credentials = pika.PlainCredentials(rabbit_user, rabbit_password)
+try:
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(os.getenv("RMQ_HOST") or 'localhost',
+                                5672,
+                                os.getenv("RMQ_VHOST") or '/',
+                                credentials=credentials))
+    channel = connection.channel()
+except (pika.exceptions.AMQPConnectionError, Exception) as err:
+    print(type(err).__name__)
+    print(err)
+    raise
+
+channel.exchange_declare(exchange=rabbit_exchange, exchange_type='direct')
+
+result = channel.queue_declare(queue='', exclusive=True)
+queue_name = result.method.queue
+
+# fetch list of routing keys
+try:
+    key_list = json.loads(os.environ['RMQ_ROUTINGKEYS'])
+except (json.decoder.JSONDecodeError, Exception) as err:
+    print(type(err).__name__)
+    print(err)
+    raise
+
+for r_key in key_list:
+    channel.queue_bind(
+        exchange=rabbit_exchange, queue=queue_name, routing_key=r_key)
+
+channel.basic_consume(
+    queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+print("listening for messages ...")
+try:
+    channel.start_consuming()
+except KeyboardInterrupt:
+    print("\n consumer force stopped")
+    try:
+        sys.exit(0)
+    except SystemExit:
+        os._exit(0)
---
+++ routing/producer/send.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+'''Job which sends messages to RabbitMQ via the direct exchange'''
+
+import json
+import os
+import string
+import sys
+import random
+import pika
+
+try:
+    n_messages = int(sys.argv[2])
+    r_key = sys.argv[1]
+except IndexError as ierr:
+    print(ierr)
+    raise
+except (ValueError, Exception) as err:
+    print(err)
+    raise
+
+# credentials
+rabbit_user = os.getenv("RMQ_USER")
+rabbit_password = os.getenv("RMQ_PASSWORD")
+# direct exchange name
+rabbit_exchange = os.getenv("RMQ_EXCHANGE") or 'directex01'
+
+# connection specs
+credentials = pika.PlainCredentials(rabbit_user, rabbit_password)
+try:
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(os.getenv("RMQ_HOST") or 'localhost',
+                                5672,
+                                os.getenv("RMQ_VHOST") or '/',
+                                credentials=credentials))
+    channel = connection.channel()
+except (pika.exceptions.AMQPConnectionError, Exception) as err:
+    print(type(err).__name__)
+    print(err)
+    raise
+
+channel.exchange_declare(exchange=rabbit_exchange, exchange_type='direct')
+
+# send messages
+COUNT = 0
+while COUNT < n_messages:
+    message = {
+        'id': COUNT,
+        'data': ''.join(random.choices(string.ascii_uppercase +
+                             string.digits, k=10))
+    }
+    channel.basic_publish(exchange=rabbit_exchange, routing_key=r_key, body=json.dumps(message))
+    print("sent message # " + str(COUNT) + " => " + json.dumps(message))
+    COUNT += 1
+connection.close()
